0cddb53dd99da2331e6439049240567e57d1d57d
Christina Greene <christina.greene@rogers.com>
2019-09-14 12:37:06 -0400

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// var carousel = document.querySelector('.photoGallery');
+// var flkty = new Flickity( carousel, {
+//    cellAlign: 'left',
+//    contain: true,
+//    wrapAround: true,
+//    prevNextButtons: false,
+//    pageDots: false,
+//    autoPlay: true
+// });
