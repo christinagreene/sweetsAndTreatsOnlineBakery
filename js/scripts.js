2cde08f839e58838118fbda329ced27cc522d802
Christina Greene <christina.greene@rogers.com>
2019-09-15 22:58:04 -0400

    HTML & CSS complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-// var carousel = document.querySelector('.photoGallery');
-// var flkty = new Flickity( carousel, {
-//    cellAlign: 'left',
-//    contain: true,
-//    wrapAround: true,
-//    prevNextButtons: false,
-//    pageDots: false,
-//    autoPlay: true
-// });
+var carousel = document.querySelector('.photoGallery');
+var flkty = new Flickity( carousel, {
+   cellAlign: 'center',
+   contain: true,
+   wrapAround: true,
+   prevNextButtons: false,
+   pageDots: false,
+   autoPlay: true,
+   accessibility: true
+});
