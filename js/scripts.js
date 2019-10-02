0bfccfe02fb7f5982759af90680500d73c657c78
Christina Greene <christina.greene@rogers.com>
2019-10-02 16:40:31 -0400

    updated comments and shop items

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ var flkty = new Flickity( carousel, {
    accessibility: true
 });
 
-// Show cart when clicking the Cart Icon
+// Show cart contents (sidebar) when clicking the cart icon
 (function(){
    const cartInfo = document.getElementById('cartInfo');
    const cart = document.getElementById('cart');
@@ -21,27 +21,31 @@ var flkty = new Flickity( carousel, {
    });
 })();
 
-// Add items to the cart
 (function(){
-   const cartBtn = document.querySelectorAll('.addToCart');
-
-   cartBtn.forEach(function(btn) {
-      btn.addEventListener('click', function(e) {
+   // 
+   const addItem = document.querySelectorAll('.addToCart');
+   
+   // Add items from the shop to the cart
+   addItem.forEach(function(button) {
+      button.addEventListener('click', function(e) {
 
          e.preventDefault();
 
          const item = {};
 
+         // locating each item name in shop to include in description of added items in cart
          let name = e.target.previousElementSibling.previousElementSibling.textContent;
 
          item.name = name;
 
+         // locating price of ea.ch item in shop to include in description and totals of items in cart
          let price = e.target.previousElementSibling.textContent;
 
          let finalPrice = price.slice(1);
 
          item.price = finalPrice;
 
+         // create div for cart items once something is added to the cart
          const cartItem = document.createElement('div');
          cartItem.classList.add('cartItemContainer');
 
@@ -59,15 +63,18 @@ var flkty = new Flickity( carousel, {
          const cart = document.getElementById('cart');
          const itemPrice = document.querySelector('.cartTotalContainer');
 
+         // items that get added to the cart to be located before the cart total in the DOM
          cart.insertBefore(cartItem, itemPrice);
+
+         // alerts the user that an item was added to the cart
          alert('You have added this item to the cart.');
 
          showTotals();
       })
    });
 
-   // to show the totals
-   function showTotals(){
+   // to show the total in the nav bar and in the cart
+   function showTotals() {
       const total = [];
       const items = document.querySelectorAll('.cartItemPrice');
 
@@ -87,4 +94,22 @@ var flkty = new Flickity( carousel, {
       document.querySelector('.itemTotal').textContent = dollarAmount;
       document.getElementById('itemCount').textContent = total.length;
    };
+
+   
+   // function deleteItem() {
+   //    const clearItem = document.getElementById('removeCartItem');
+      
+   //    clearItem.forEach(function(clearButton) {
+   //       clearButton.addEventListener('click', function(event) {
+            
+   //          e.preventDefault();
+            
+   //       })
+   //    })
+   // }
+   
+   // function deleteCart() {
+   //    const clearCart = document.getElementById('clearCart');
+
+   // }
 })();
