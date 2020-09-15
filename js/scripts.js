1dc60561c821a012e8167b4f5b4f22521b78a380
Christina Greene <christina.greene@rogers.com>
2020-09-15 00:21:10 -0400

    Functionalities fixed/updated and responsive added

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
-// flickety for gallery
-var carousel = document.querySelector('.photoGallery');
-var flkty = new Flickity( carousel, {
+if (document.readyState == 'loading') {
+   document.addEventListener('DOMContentLoaded', ready);
+} else {
+   ready();
+};
+
+
+// Flickity for gallery
+const carousel = document.querySelector('.photoGallery');
+const flkty = new Flickity(carousel, {
    cellAlign: 'center',
    contain: true,
    wrapAround: true,
@@ -10,101 +17,135 @@ var flkty = new Flickity( carousel, {
    accessibility: true
 });
 
+
+// Hamburger menu for smaller screens
+let hamburger = document.querySelector('.menuToggle');
+let menu = document.querySelector('.siteNav');
+
+hamburger.addEventListener('click', function(){
+   hamburger.classList.toggle('openMenu');
+   menu.classList.toggle('active');
+});
+
+
+/////// CART FUNCTIONALITY ///////
 // Show cart contents (sidebar) when clicking the cart icon
-(function(){
+(function () {
    const cartInfo = document.getElementById('cartInfo');
    const cart = document.getElementById('cart');
 
-   cartInfo.addEventListener('click', function(){
+   cartInfo.addEventListener('click', function () {
       cart.classList.toggle('showCart');
       cartInfo.classList.toggle('cartInfoOpen');
    });
-})(); // end of show cart contents
+})(); 
 
-(function(){
-   const cart = document.getElementById('cart');
-   const addItem = document.querySelectorAll('.addToCart');
-   const deleteItem = document.getElementById('removeCartItem');
-   const deleteCart = document.getElementById('clearCart');
+// Add event listener on buttons and lead to appropriate functions
+function ready() {
+   const removeCartItemButtons = document.getElementsByClassName('removeCartItem');
+   for (let i = 0; i < removeCartItemButtons.length; i++) {
+      const button = removeCartItemButtons[i];
+      button.addEventListener('click', removeCartItem);
+   };
+
+   const quantityInputs = document.getElementsByClassName('itemQuantity');
+   for (let i = 0; i < quantityInputs.length; i++) {
+      const input = quantityInputs[i]
+      input.addEventListener('change', quantityChanged)
+   }
+
+   const addToCartButtons = document.getElementsByClassName('addToCart');
+   for (let i = 0; i < addToCartButtons.length; i++) {
+      const button = addToCartButtons[i];
+      button.addEventListener('click', addItemClicked);
+   };
+};
+
+// Removes individual items from cart & updates total
+function removeCartItem(e) {
+   let removeButton = e.target;
+   removeButton.parentElement.parentElement.parentElement.remove();
+   updateCartTotal();
+}
+
+// Prevents quantity for item in cart from going below 1 on an item & updates total on change
+function quantityChanged(e) {
+   let input = e.target;
+   if (isNaN(input.value) || input.value <= 0) {
+      input.value = 1;
+   }
+   updateCartTotal();
+}
+
+// Adds item to cart & updates total
+function addItemClicked(e) {
+   e.preventDefault();
+   let addButton = e.target;
    
-   // Add items from the shop to the cart
-   addItem.forEach(function(button) {
-      button.addEventListener('click', function(e) {
+   let itemName = addButton.previousElementSibling.previousElementSibling.innerText;
 
-         e.preventDefault();
+   let priceValue = addButton.previousElementSibling.innerText;
+   let itemPrice = parseFloat(priceValue.replace('$', ''));
 
-         const item = {};
+   addItemToCart(itemName, itemPrice);
+   updateCartTotal();
+}
 
-         // locating each item name in shop to include in description of added items in cart
-         let name = e.target.previousElementSibling.previousElementSibling.textContent;
+// Adds dynamic content about items, alerts for confirming addition of item and preventing from adding more than one of same item from main shop page
+function addItemToCart(itemName, itemPrice) {
 
-         item.name = name;
+   let cartItemContainerDiv = document.createElement('div');
+   cartItemContainerDiv.classList.add('cartItemContainer');
+   let cartItemsDiv = document.getElementsByClassName('cartItems')[0];
 
-         // locating price of each item in shop to include in description and totals of items in cart
-         let price = e.target.previousElementSibling.textContent;
-
-         let finalPrice = price.slice(1);
-
-         item.price = finalPrice;
-
-         // create div for cart items once something is added to the cart
-         const cartItem = document.createElement('div');
-         cartItem.classList.add('cartItemContainer');
-
-         cartItem.innerHTML = `
-            <div class="cartItemText">
-               <p class="cartItemTitle" id="cartItemTitle">${item.name}</p>
-               <p class="cartItemPrice">$<span id="cartItemPrice">${item.price}</span></p>
-            </div>
-            <div class="removeFromCart">
-               <a href="#" id="removeCartItem" class="removeCartItem"><i class="fas fa-trash-alt"></i></a>
-            </div>
-         `;
-
-         // select cart
-         const itemPrice = document.querySelector('.cartTotalContainer');
-
-         // items that get added to the cart to be located before the cart total in the DOM
-         cart.insertBefore(cartItem, itemPrice);
-
-         // alerts the user that an item was added to the cart
-         alert('You have added this item to the cart.');
-
-         showTotals();
-
-      })
-   });
-
-   // REMOVE ITEM
-   // remove selected .cartItemContainer on click of trash can called removeCartItem = removeItem()
-
+   let cartItemNames = cartItemsDiv.getElementsByClassName('cartItemTitle');
    
+   for (let i = 0; i < cartItemNames.length; i++) {
+      if (cartItemNames[i].innerText == itemName) {
+         alert('Oops! You have already added this item to the cart.')
+         return
+      }
+   }
 
+   let cartItemContents = `
+      <div class="cartItemText">
+         <p class="cartItemTitle">${itemName}</p>
+         <p>$<span class="cartItemPrice">${itemPrice}</span></p>
+      </div>
+      <div class="cartItemQuantity">
+         <p>x <input class="itemQuantity" type="number" value="1"></p>
+      </div>
+      <div class="remofcartveFromCart">
+         <a href="#" class="removeCartItem"><i class="fas fa-trash-alt"></i></a>
+      </div>`;
 
-   // CLEAR CART
-   // run removeAllItems() to remove all .cartItemContainer
+   cartItemContainerDiv.innerHTML = cartItemContents;
+   cartItemsDiv.append(cartItemContainerDiv);
+   cartItemContainerDiv.getElementsByClassName('removeCartItem')[0].addEventListener('click', removeCartItem);
+   cartItemContainerDiv.getElementsByClassName('itemQuantity')[0].addEventListener('change', quantityChanged);
 
+   alert('We have added this delicious item to your cart!');
+}
 
+// Updates the total price of cart contents
+function updateCartTotal() {
+   const cartItemContainers = document.getElementsByClassName('cartItemContainer');
 
-   // function to show the total in the nav bar and in the cart
-   function showTotals() {
-      const total = [];
-      const items = document.querySelectorAll('.cartItemPrice');
+   let total = 0;
+   let totalDollarAmount = '0.00';
 
-      items.forEach(function(item) {
-         total.push(parseFloat(item.textContent.slice(1)));
-      });
+   for (let i = 0; i < cartItemContainers.length; i++) {
+      let cartItemInfo = cartItemContainers[i];
+      let priceElement = cartItemInfo.getElementsByClassName('cartItemPrice')[0];
+      let quantityElement = cartItemInfo.getElementsByClassName('itemQuantity')[0];
+      
+      let price = priceElement.innerText;
+      let quantity = parseInt(quantityElement.value);
+      total = total + (price * quantity);
+      totalDollarAmount = total.toFixed(2);
+   }
 
-      const totalCost = total.reduce(function(total, item) {
-         total += item;
-         return total;
-      }, 0)
-
-      const dollarAmount = totalCost.toFixed(2);
-
-      document.getElementById('cartTotal').textContent = dollarAmount;
-      document.querySelector('.itemTotal').textContent = dollarAmount;
-      document.getElementById('itemCount').textContent = total.length;
-   }; // end of showTotals function
-
-})(); // end of add and delete cart functions
+   // appends total price to menu and cart sidebar
+   document.getElementsByClassName('cartTotal')[0].innerText = totalDollarAmount;
+   document.getElementsByClassName('itemTotal')[0].innerText = totalDollarAmount;
+}
